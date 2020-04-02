3712f7d049d342eb6dff377d92dcbd55cca1e7f2
boritek22 <53436656+boritek22@users.noreply.github.com>
2020-04-02 23:42:32 +0200

    Kép és videó manuális slideshow-ja
    Ha egy receptnél kép és videó is van, akkor beteszi óket egy manuálisan vezérelhető slideshow-ba, hogy ne egymás alatt jelenjenek meg, hanem szépen egymás után, mint egy rendes receptoldalon.

---
+++ receptmedia/database.php
@@ -0,0 +1,24 @@
+<?php
+
+function kapcsolodas($kapcsolati_szoveg, $felhasznalonev = '', $jelszo = '') {
+  $pdo = new PDO($kapcsolati_szoveg, $felhasznalonev, $jelszo);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  return $pdo;
+}
+
+function lekerdezes($kapcsolat, $sql, $parameterek = []) {
+  $stmt = $kapcsolat->prepare($sql);
+  $stmt->execute($parameterek);
+  return $stmt->fetchAll();
+}
+
+function vegrehajtas($kapcsolat, $sql, $parameterek = []) {
+  return $kapcsolat
+    ->prepare($sql)
+    ->execute($parameterek);
+}
+
+$db = kapcsolodas(
+  "mysql:host=localhost;dbname=wf2_kojg0x;charset=utf8",
+  "kojg0x", "kojg0x"
+);
